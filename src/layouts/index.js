f5482450f3e7a26ab6c0a1c7d5ce967c6f2066d9
Loïc Mahieu <mahieuloic@gmail.com>
2017-11-17 16:16:02 +0100

    remove to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,74 @@
+
+import styled from 'styled-components'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    {children()}
+// Assets
+import bgStars from '../assets/stars.svg'
+import bgOctopus from '../assets/octopus.svg'
+import WaveLt from '../assets/wave-lt.svg'
+import WaveDk from '../assets/wave-dk.svg'
+import logoIgloo from '../assets/igloo.svg'
+
+// Lib
+import { colors } from '../lib/constants'
+import { fontFamily } from '../lib/Fonts'
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 940px;
+  padding: 15px;
+`
+
+const TemplateWrapper = styled(({ className, children }) => (
+  <div className={className}>
+    <Wrapper>
+      {children()}
+    </Wrapper>
   </div>
-)
+))`
+  min-height: 100vh;
+  display: flex;
+
+  background:
+    url(${logoIgloo}) no-repeat bottom 75px right 30px,
+    url(${WaveLt}) repeat-x bottom,
+    url(${bgOctopus}) no-repeat bottom center,
+    url(${WaveDk})  repeat-x -15px bottom,
+    url(${bgStars}) repeat-x,
+    ${colors.primary};
+
+  color: ${colors.light};
+  ${fontFamily({weight: 'book', size: '14px'})};
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+
+  // MediaQueries
+  @media screen and (max-height: 940px) {
+    background:
+      url(${logoIgloo}) no-repeat bottom 75px right 30px,
+      url(${WaveLt}) repeat-x bottom,
+      url(${WaveDk})  repeat-x -15px bottom,
+      url(${bgStars}) repeat-x,
+      ${colors.primary};
+  }
+
+  @media screen and (max-height: 760px) {
+  background:
+    url(${WaveLt}) repeat-x bottom,
+    url(${bgStars}) repeat-x,
+    url(${WaveDk})  repeat-x -15px bottom,
+    url(${bgStars}) repeat-x,
+    ${colors.primary};
+  }
+
+  @media screen and (max-height: 700px) {
+  background:
+    url(${bgStars}) repeat-x,
+    ${colors.primary};
+  }
+`
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,6 @@ import styled from 'styled-components'
 // Deps
 import '../../node_modules/normalize.css/normalize.css'
 
-// Assets
-import bgStars from '../assets/stars.svg'
-import bgOctopus from '../assets/octopus.svg'
-import WaveLt from '../assets/wave-lt.svg'
-import WaveDk from '../assets/wave-dk.svg'
-import logoIgloo from '../assets/igloo.svg'
-
-// Lib
-import { colors } from '../lib/constants'
-import { fontFamily } from '../lib/Fonts'
-
 // Components
 import Button from '../components/ui/Button'
 import Header from '../components/Header'
@@ -30,66 +19,16 @@ const Desc = styled.div`
   text-align: center;
 `
 
-const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 940px;
-  padding: 15px;
-`
-
-const IndexPage = styled(({className}) => (
-  <div className={className}>
+const IndexPage = ({className}) => (
+  <div>
     <Helmet title='Maintenance' />
-    <Wrapper>
-      <Header error='503' title='Service unavailable' />
-      <Desc>
-        The server is currently unavailable. Please try to go back or try again later.
-      </Desc>
-      <Footer><Button>Go back</Button></Footer>
-      <TableInfo />
-    </Wrapper>
+    <Header error='503' title='Service unavailable' />
+    <Desc>
+      The server is currently unavailable. Please try to go back or try again later.
+    </Desc>
+    <Footer><Button>Go back</Button></Footer>
+    <TableInfo />
   </div>
-))`
-  min-height: 100vh;
-  display: flex;
-
-  background:
-    url(${logoIgloo}) no-repeat bottom 75px right 30px,
-    url(${WaveLt}) repeat-x bottom,
-    url(${bgOctopus}) no-repeat bottom center,
-    url(${WaveDk})  repeat-x -15px bottom,
-    url(${bgStars}) repeat-x,
-    ${colors.primary};
-
-  color: ${colors.light};
-  ${fontFamily({weight: 'book', size: '14px'})};
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-
-  // MediaQueries
-  @media screen and (max-height: 940px) {
-    background:
-      url(${logoIgloo}) no-repeat bottom 75px right 30px,
-      url(${WaveLt}) repeat-x bottom,
-      url(${WaveDk})  repeat-x -15px bottom,
-      url(${bgStars}) repeat-x,
-      ${colors.primary};
-  }
-
-  @media screen and (max-height: 760px) {
-   background:
-     url(${WaveLt}) repeat-x bottom,
-     url(${bgStars}) repeat-x,
-     url(${WaveDk})  repeat-x -15px bottom,
-     url(${bgStars}) repeat-x,
-     ${colors.primary};
-  }
-
-  @media screen and (max-height: 700px) {
-   background:
-     url(${bgStars}) repeat-x,
-     ${colors.primary};
-  }
-`
+)
 
 export default IndexPage
