5930e4b678c220668a706fb24560338630f11662
Loïc Mahieu <mahieuloic@gmail.com>
2017-11-17 16:32:01 +0100

    Introduce 404 page

---
+++ src/components/Desc.js
@@ -0,0 +1,12 @@
+
+import styled from 'styled-components'
+
+const Desc = styled.div`
+  font-size: 18px;
+  line-height: 23px;
+  margin: 0 auto;
+  max-width: 460px;
+  text-align: center;
+`
+
+export default Desc
--- src/pages/404.js
+++ src/pages/404.js
@@ -11,16 +11,16 @@ import Footer from '../components/Footer'
 import TableInfo from '../components/TableInfo'
 import Desc from '../components/Desc'
 
-const IndexPage = ({className}) => (
+const Error404Page = ({className}) => (
   <div>
-    <Helmet title='Maintenance' />
-    <Header error='503' title='Service unavailable' />
+    <Helmet title='Error 404' />
+    <Header error='404' title='Not Found' />
     <Desc>
-      The server is currently unavailable. Please try to go back or try again later.
+      Blabla
     </Desc>
     <Footer><Button>Go back</Button></Footer>
     <TableInfo />
   </div>
 )
 
-export default IndexPage
+export default Error404Page
